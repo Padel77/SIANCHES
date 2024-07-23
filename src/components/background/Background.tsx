17da99df97f72e3525f92727f0d45764839e6488
Adel Assem <46504605+Padel77@users.noreply.github.com>
2024-07-23 16:16:39 +0300

    Update Background.tsx

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -33,7 +33,7 @@ export const Background: React.FC<BackgroundProps> = ({
       >
         {title}
         {showButton && <Link
-          href="/Properties"
+          href="/properties"
           className="absolute flex items-center justify-center  md:my-6 gap-2 top-2/3 p-3 text-xs mx-auto bg-[#F8F8F7] text-[#2D2D2D] border"
         >
           Explore Properties <ArrowUpRight size={16} />
