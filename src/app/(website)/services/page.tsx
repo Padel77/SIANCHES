5666f65b76d9ae916f89a00070eaf8cf056257e2
Adel Assem <46504605+Padel77@users.noreply.github.com>
2024-07-23 16:34:23 +0300

    Update page.tsx

--- src/app/(website)/services/page.tsx
+++ src/app/(website)/services/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     <>
       <Background
         imageUrl={"https://s3-alpha-sig.figma.com/img/48bf/0921/6999851aadf02a908928fba17e60d4d4?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Vn8RLfFkH6H2DACN3~Ko7Iz-4BE4RIojcYSPU4ZqiNdTn2TQHKm049GYJFGO6GeWAQokLlQ-04YwrWKTFnQlBWx6UxM5Cnln1V2lALrglqAfR9Wz-Qcnt0UKG2mgRWiBnDy-7y9Uron~0RjHSiDbuVeZ1lXA9lCSrFQXQrmbaTDvgCDx5X-j40fBkUMdO-zls99-JJa61jxBpNa2TIJJGPuyyadLATQCwPcEhc-3n3y3hLa5jpbpTnopQK2pXRVeuBN4j4z13DqUt~YAqETy8opwPaAZNwokN8eG96wXNRn9sRQnI--ooqwSS~eGmroDL-nE4qVcoSUNCiB8a7Ioxg__"}
-        className="flex absolute h-full text-center items-center text-5xl left-[170px] top-[285px]     italic font-bold "
+        className="flex absolute h-full text-center items-center justify-center text-5xl md:left-[170px] md:top-[285px]  italic font-bold "
         title={"Our services"}
         showButton={false}
       />
